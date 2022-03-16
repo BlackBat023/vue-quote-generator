cb04622a5f948dab549d0c89b84546b9952ffaa8
Mauritz Vervaart <mjver@mweb.co.za>
2022-03-16 23:27:22 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <Header>
+        <h1>{{title}}</h1>
+    </Header>
+</template>
+
+<script>
+export default {
+    props: ['title']
+}
+</script>
+
+<style lang="scss" scoped>
+Header {
+    padding: 24px 32px;
+    display: flex;
+    justify-content: center;
+}
+
+h1 {
+    font-size: 32px;
+    color: var(--dark);
+    font-weight: 700;
+    text-align: center;
+    text-transform: uppercase;
+    padding-bottom: 8px;
+    border-bottom: 5px solid var(--orimary);
+}
+
+</style>
